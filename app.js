e657c2694e95606235a23abe3a542799b61c0877
vaibhav <vpatil7382@gmail.com>
2023-06-29 13:12:35 +0530

    parcel packege install

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
-// import React from "react";
-// import reactDom from "react-dom";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-    React.createElement("h3", {}, "I am a h3 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-    React.createElement("h3", {}, "I am a h3 tag"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
 
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-// const heading = React.createElement("h1", {id: "heading"}, "hello world from React");
+root.render(heading);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am a h1 tag"),
+//     React.createElement("h2", {}, "I am a h2 tag"),
+//     React.createElement("h3", {}, "I am a h3 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am a h1 tag"),
+//     React.createElement("h2", {}, "I am a h2 tag"),
+//     React.createElement("h3", {}, "I am a h3 tag"),
+//   ]),
+// ]);
+
+// console.log(parent);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
+// root.render(parent);

133dc5654bec087978af70ae5e52f4ac601d9f00
vaibhav <vpatil7382@gmail.com>
2023-07-01 12:55:45 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
 
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", {}, "I am a h1 tag"),
-//     React.createElement("h2", {}, "I am a h2 tag"),
-//     React.createElement("h3", {}, "I am a h3 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", {}, "I am a h1 tag"),
-//     React.createElement("h2", {}, "I am a h2 tag"),
-//     React.createElement("h3", {}, "I am a h3 tag"),
-//   ]),
-// ]);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "vaibhav is here"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+    React.createElement("h3", {}, "I am a h3 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+    React.createElement("h3", {}, "I am a h3 tag"),
+  ]),
+]);
 
-// console.log(parent);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -3,20 +3,26 @@
 // const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", {}, "I am a h1 tag"),
-//     React.createElement("h2", {}, "I am a h2 tag"),
-//     React.createElement("h3", {}, "I am a h3 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", {}, "I am a h1 tag"),
-//     React.createElement("h2", {}, "I am a h2 tag"),
-//     React.createElement("h3", {}, "I am a h3 tag"),
-//   ]),
-// ]);
-// console.log(parent);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+        React.createElement("h3", {}, "I am a h3 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I am a h1 tag"),
+        React.createElement("h2", {}, "I am a h2 tag"),
+        React.createElement("h3", {}, "I am a h3 tag")
+    ])
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
 
 //# sourceMappingURL=index.7c0ccee6.js.map

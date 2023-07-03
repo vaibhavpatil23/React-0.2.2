c78d24f1a2c527044bc9e13e86165c412d9d69a6
vaibhav <vpatil7382@gmail.com>
2023-07-03 11:52:50 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
+const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "vaibhav is here"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-    React.createElement("h3", {}, "I am a h3 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-    React.createElement("h3", {}, "I am a h3 tag"),
-  ]),
-]);
-
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
+

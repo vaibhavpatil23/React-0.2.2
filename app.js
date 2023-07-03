c6cd07430047c269884bfae3d0d3b75ef9726bc1
vaibhav <vpatil7382@gmail.com>
2023-07-03 12:39:10 +0530

    start the jsx

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
 
+// const heading = React.createElement("h1", { id: "heading" }, "vaibhav patil");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// jsx
+const jsxheading = <h1 className="head" tabIndex="5">Hiii Vaibhav</h1>;
+
+console.log(
+  jsxheading
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(jsxheading);

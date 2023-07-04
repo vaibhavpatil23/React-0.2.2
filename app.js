41d80efe15ef28ba94b0df042e1be577c22306a5
vaibhav <vpatil7382@gmail.com>
2023-07-04 13:08:45 +0530

    restorant app create

--- app.js
+++ app.js
@@ -1,24 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// jsx
-const jsxheading = (
-  <h1 className="head" tabIndex="5">
-    Hiii Vaibhav
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt=""
+        />
+      </div>
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Card</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// console.log(jsxheading);
-// const root = ReactDOM.createRoot(document.getElementById("root")); 
-// root.render(jsxheading);
+const RestaurentCard = () => {
+  return (
+    <div className="rest-card">
+      <img
+      className="rest-logo"
+       src="https://images.pexels.com/photos/313716/pexels-photo-313716.jpeg" alt="" />
+      <h3>Meghana Food</h3>
+      <h4>Biryani, North Indian,Asian</h4>
+      <h4>4.4 start</h4>
+      <h4>38 minuts</h4>
+    </div>
+  );
+};
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      <RestaurentCard />
+      </div>
+    </div>
+  );
+};
 
-// react componant
-const Headingcomponant = () =>(
-<div id="container">
-  <title/>
-  <h1 className="heading"> React is a functinal componant</h1>
-</div>
-)
-const root = ReactDOM.createRoot(document.getElementById("root")); 
-root.render(<Headingcomponant/>);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
